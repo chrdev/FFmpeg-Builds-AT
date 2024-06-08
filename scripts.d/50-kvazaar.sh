daa97d7474c66e527bcaf14c931f66a43082d19b
chrdev <chrdev@zoho.com>
2024-06-09 01:06:26 +0800

    Add aac_at codec. Use 7z. Remove arm support. Reschedule build.

--- scripts.d/50-kvazaar.sh
+++ scripts.d/50-kvazaar.sh
@@ -4,7 +4,7 @@ SCRIPT_REPO="https://github.com/ultravideo/kvazaar.git"
 SCRIPT_COMMIT="526c8db10e263719532afe6b0d01cf570a5d1b20"
 
 ffbuild_enabled() {
-    return 0
+    return -1
 }
 
 ffbuild_dockerbuild() {
